791de968868e3cebca7a06aa7b908f6863a2f53c
Alexander Shvets <neochief@shvetsgroup.com>
2018-05-01 19:18:24 +0300

    Iterator example.

--- src/RefactoringGuru/Iterator/RealLife/IteratorRealLife.php
+++ src/RefactoringGuru/Iterator/RealLife/IteratorRealLife.php
@@ -5,9 +5,150 @@ namespace RefactoringGuru\Iterator\RealLife;
 /**
  * Iterator Design Pattern
  *
- * Intent:
+ * Intent: Provide a way to access the elements of an aggregate objects
+ * without exposing its underlying representation.
  *
- * Example:
+ * Example: The Iterator pattern allows easy access to CSV files.
  */
 
-// TODO
+/**
+ * CSV File Iterator
+ *
+ * @author Josh Lockhart
+ */
+class CsvIterator implements \Iterator
+{
+    const ROW_SIZE = 4096;
+
+    /**
+     * The pointer to the cvs file.
+     *
+     * @var resource
+     * @access protected
+     */
+    protected $filePointer = null;
+
+    /**
+     * The current element, which will
+     * be returned on each iteration.
+     *
+     * @var array
+     * @access protected
+     */
+    protected $currentElement = null;
+
+    /**
+     * The row counter.
+     *
+     * @var int
+     * @access protected
+     */
+    protected $rowCounter = null;
+
+    /**
+     * The delimiter for the csv file.
+     *
+     * @var str
+     * @access protected
+     */
+    protected $delimiter = null;
+
+    /**
+     * This is the constructor.It try to open the csv file.The method throws an exception
+     * on failure.
+     *
+     * @access public
+     * @param string $file The csv file.
+     * @param string $delimiter The delimiter.
+     *
+     * @throws \Exception
+     */
+    public function __construct($file, $delimiter = ',')
+    {
+        try {
+            $this->filePointer = fopen($file, 'rb');
+            $this->delimiter = $delimiter;
+        } catch (\Exception $e) {
+            throw new \Exception('The file "'.$file.'" cannot be read.');
+        }
+    }
+
+    /**
+     * This method resets the file pointer.
+     *
+     * @access public
+     */
+    public function rewind()
+    {
+        $this->rowCounter = 0;
+        rewind($this->filePointer);
+    }
+
+    /**
+     * This method returns the current csv row as a 2 dimensional array
+     *
+     * @access public
+     * @return array The current csv row as a 2 dimensional array
+     */
+    public function current()
+    {
+        $this->currentElement = fgetcsv($this->filePointer, self::ROW_SIZE, $this->delimiter);
+        $this->rowCounter++;
+
+        return $this->currentElement;
+    }
+
+    /**
+     * This method returns the current row number.
+     *
+     * @access public
+     * @return int The current row number
+     */
+    public function key()
+    {
+        return $this->rowCounter;
+    }
+
+    /**
+     * This method checks if the end of file is reached.
+     *
+     * @access public
+     * @return boolean Returns true on EOF reached, false otherwise.
+     */
+    public function next()
+    {
+        if (is_resource($this->filePointer)) {
+            return ! feof($this->filePointer);
+        }
+
+        return false;
+    }
+
+    /**
+     * This method checks if the next row is a valid row.
+     *
+     * @access public
+     * @return boolean If the next row is a valid row.
+     */
+    public function valid()
+    {
+        if (! $this->next()) {
+            if (is_resource($this->filePointer)) {
+                fclose($this->filePointer);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+/**
+ * Client code.
+ */
+$csv = new CsvIterator('cats.csv');
+
+foreach ($csv as $key => $row) {
+    print_r($row);
+}
