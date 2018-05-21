e4580ad788a895c043b3be72a5eb4af3c414aa59
Alexander Shvets <neochief@shvetsgroup.com>
2018-05-21 17:30:40 +0300

    Updated Structure texts.

--- src/RefactoringGuru/Visitor/Structure/VisitorStructure.php
+++ src/RefactoringGuru/Visitor/Structure/VisitorStructure.php
@@ -8,22 +8,11 @@ namespace RefactoringGuru\Visitor\Structure;
  * Intent: Represent an operation to be performed over elements of an object
  * structure. The Visitor pattern lets you define a new operation without
  * changing the classes of the elements on which it operates.
- *
- *
- * Visitor Design Pattern
- *
- * Intent: Represent an operation to be performed over elements of an object
- * structure. The Visitor pattern lets you define a new operation without
- * changing the classes of the elements on which it operates.
  */
 
 /**
  * EN: The Component interface declares an `accept` method that should take the
  * base visitor interface as an argument.
- *
- *
- * The Component interface declares an `accept` method that should take the base
- * visitor interface as an argument.
  *
  * RU: Интерфейс компонентов должен объявлять метод, принимающий базовый
  * интерфейс посетителей в качестве аргумента.
@@ -38,10 +27,6 @@ interface Component
  * way that it calls the visitor's method, corresponding to the component's
  * class.
  *
- *
- * Each Concrete Component must implement the `accept` method in a such a way
- * that it calls the visitor's method, corresponding to the component's class.
- *
  * RU: Каждый конкретный компонент должен реализовать метод `accept` таким
  * образом, чтобы вызвать метод посетителя, соотвествующий текущему классу.
  */
@@ -49,11 +34,6 @@ class ConcreteComponentA implements Component
 {
     /**
      * EN: Note that we're calling `visitConcreteComponentA`, which matches the
-     * current class name. This way we let the visitor know the class of the
-     * component it works with.
-     *
-     *
-     * Note that we're calling `visitConcreteComponentA`, which matches the
      * current class name. This way we let the visitor know the class of the
      * component it works with.
      *
@@ -71,11 +51,6 @@ class ConcreteComponentA implements Component
      * their base class or interface. The Visitor is still able to use these
      * methods since it's aware of the component's concrete class.
      *
-     *
-     * Concrete Components may have special methods that don't exist in their
-     * base class or interface. The Visitor is still able to use these methods
-     * since it's aware of the component's concrete class.
-     *
      * RU: Конкретные Компоненты могут иметь особые методы, не объявленные в их
      * базовом классе или интерфейсе. Несмотря на это, посетитель всё-равно
      * сможет их использовать, так как знает конкретный класс компонента.
@@ -90,9 +65,6 @@ class ConcreteComponentB implements Component
 {
     /**
      * EN: Same here: visitConcreteComponentB => ConcreteComponentB
-     *
-     *
-     * Same here: visitConcreteComponentB => ConcreteComponentB
      *
      * RU: То же самое: visitConcreteComponentB => ConcreteComponentB
      */
@@ -112,11 +84,6 @@ class ConcreteComponentB implements Component
  * to component classes. The signature of a visiting method allows the visitor
  * to identify the exact class of the component that it's dealing with.
  *
- *
- * The Visitor Interface declares a set of visiting methods that correspond to
- * component classes. The signature of a visiting method allows the visitor to
- * identify the exact class of the component that it's dealing with.
- *
  * RU: Интерфейс Посетителя объявляет набор методов посещения для каждого класса
  * компонента. Сигнатуры этих методов позволяют посетителю определить конкретный
  * класс компонента, с которым он работает.
@@ -131,15 +98,6 @@ interface Visitor
 /**
  * EN: Concrete Visitors implement several versions of the same algorithm, which
  * can work with all concrete component classes.
- *
- * You can experience the biggest benefit of the Visitor pattern when using it
- * with a complex object structure, such as a Composite tree. In this case, it
- * might be helpful to store some intermediate state of the algorithm while
- * executing visitor's methods over various objects of the structure.
- *
- *
- * Concrete Visitors implement several versions of the same algorithm, which can
- * work with all concrete component classes.
  *
  * You can experience the biggest benefit of the Visitor pattern when using it
  * with a complex object structure, such as a Composite tree. In this case, it
@@ -186,11 +144,6 @@ class ConcreteVisitor2 implements Visitor
  * without figuring out their concrete classes. The accept operation directs a
  * call to the appropriate operation in the visitor object.
  *
- *
- * The client code can run visitor operations over any set of elements without
- * figuring out their concrete classes. The accept operation directs a call to
- * the appropriate operation in the visitor object.
- *
  * RU: Клиентский код может запускать операции посетителя над любым набором
  * компонентов, не привязываясь к их конкретным классам. Метод `accept`
  * позаботится о том, чтобы вызвать метод посетителя, соответствующий данному
@@ -199,17 +152,9 @@ class ConcreteVisitor2 implements Visitor
 function clientCode(array $components, Visitor $visitor)
 {
     // ...
-    //
-    //
-    // ...
     foreach ($components as $component) {
         $component->accept($visitor);
     }
-    // ...
-    //
-    //
-    //
-    //
     // ...
 }
 
