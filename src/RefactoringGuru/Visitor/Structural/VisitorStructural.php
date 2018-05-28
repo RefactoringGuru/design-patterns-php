0f089ead2ed1b85022d0dea4e1ed0628c89319a1
Reducibly <39105696+reducibly@users.noreply.github.com>
2018-05-28 20:47:03 +0200

    Proofreading: VisitorStructural.php

--- src/RefactoringGuru/Visitor/Structural/VisitorStructural.php
+++ src/RefactoringGuru/Visitor/Structural/VisitorStructural.php
@@ -23,8 +23,8 @@ interface Component
 }
 
 /**
- * EN: Each Concrete Component must implement the `accept` method in a such a
- * way that it calls the visitor's method, corresponding to the component's
+ * EN: Each Concrete Component must implement the `accept` method in such a
+ * way that it calls the visitor's method corresponding to the component's
  * class.
  *
  * RU: Каждый конкретный компонент должен реализовать метод `accept` таким
