6a1a2c4858f33bed9e01230b1867ec85b1194e6e
Reducibly <39105696+reducibly@users.noreply.github.com>
2018-05-29 18:57:28 +0200

    Proofreading: ProxyStructural.php - update

--- src/RefactoringGuru/Proxy/Structural/ProxyStructural.php
+++ src/RefactoringGuru/Proxy/Structural/ProxyStructural.php
@@ -23,7 +23,7 @@ interface Subject
  * The RealSubject contains some core business logic. Usually, RealSubjects are
  * capable of doing some useful work which may also be very slow or sensitive -
  * e.g. correcting input data. A Proxy can solve these issues without any 
- * changes to the real subject's code.
+ * changes to the RealSubject's code.
  */
 class RealSubject implements Subject
 {
