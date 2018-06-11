9184b345a34ac3069ef3ddae22c49793cfd98558
WTig <40171607+WTig@users.noreply.github.com>
2018-06-11 20:56:42 +0600

    Update AbstractFactoryStructural.php

--- src/RefactoringGuru/AbstractFactory/Structural/AbstractFactoryStructural.php
+++ src/RefactoringGuru/AbstractFactory/Structural/AbstractFactoryStructural.php
@@ -3,19 +3,32 @@
 namespace RefactoringGuru\AbstractFactory\Structural;
 
 /**
- * Abstract Factory Design Pattern
+ * EN: Abstract Factory Design Pattern
  *
  * Intent: Provide an interface for creating families of related or dependent
  * objects without specifying their concrete classes.
+ *
+ * RU: Паттерн Абстрактная Фабрика
+ *
+ * Назначение: Предоставляет интерфейс для создания семейств связанных или
+ * зависимых объектов, без привязки к их конкретным классам.
  */
 
 /**
+ * EN: 
  * The Abstract Factory interface declares a set of methods that return
  * different abstract products. These products are called a family and are
  * related by a high-level theme or concept. Products of one family are usually
  * able to collaborate among themselves. A family of products may have several
  * variations, but the products of one variation are incompatible with products
  * of another.
+ *
+ * RU:
+ * Интерфейс Абстрактной Фабрики объявляет набор методов, которые
+ * возвращают различные абстрактные продукты. Эти продукты называются семейством
+ * и связаны темой или концепцией высокого уровня. Продукты одного семейства обычно
+ * могут взаимодействовать между собой. Семейство продуктов может иметь несколько 
+ * вариаций, но продукты одной вариации несовместимы с продуктами другой.
  */
 interface AbstractFactory
 {
@@ -25,10 +38,18 @@ interface AbstractFactory
 }
 
 /**
+ * EN:
  * Concrete Factories produce a family of products that belong to a single
  * variation. The factory guarantees that resulting products are compatible.
  * Note that signatures of the Concrete Factory's methods return an abstract
  * product, while inside the method a concrete product is instantiated.
+ *
+ * RU:
+ * Конкретная Фабрика производит семейство продуктов одной вариации.
+ * Фабрика гарантирует совместимость полученных продуктов. 
+ * Обратите внимание, что сигнатуры методов Конкретной Фабрики возвращают
+ * абстрактный продукт, в то время как внутри метода создается экземпляр 
+ * конкретного продукта.
  */
 class ConcreteFactory1 implements AbstractFactory
 {
@@ -44,7 +65,11 @@ class ConcreteFactory1 implements AbstractFactory
 }
 
 /**
+ * EN:
  * Each Concrete Factory has a corresponding product variation.
+ *
+ * RU:
+ * Каждая Конкретная Фабрика имеет соответствующую вариацию продукта.
  */
 class ConcreteFactory2 implements AbstractFactory
 {
