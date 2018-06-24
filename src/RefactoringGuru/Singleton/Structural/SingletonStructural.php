594d1f756fdc3379ea6962efe8bc576bac1848ae
WTig <40171607+WTig@users.noreply.github.com>
2018-06-24 14:57:34 +0600

    Update SingletonStructural.php

--- src/RefactoringGuru/Singleton/Structural/SingletonStructural.php
+++ src/RefactoringGuru/Singleton/Structural/SingletonStructural.php
@@ -3,33 +3,55 @@
 namespace RefactoringGuru\Singleton\Structural;
 
 /**
- * Singleton Design Pattern
+ * EN: Singleton Design Pattern
  *
  * Intent: Ensure that a class has a single instance, and provide a global point
  * of access to it.
+ *
+ * RU: Паттерн Одиночка
+ * Назначение: Гарантирует существование единственного экземпляра класса и предоставляет
+ * глобальную точку доступа к нему.
  */
 
 /**
+ * EN:
  * The Singleton class defines the `getInstance` method that lets clients access
  * the unique singleton instance.
+ *
+ * RU:
+ * Класс Одиночка предоставляет метод getInstance, который позволяет клиентам получить доступ
+ * к уникальному экземпляру одиночки.
  */
 class Singleton
 {
     private static $instances = [];
 
     /**
+     * EN:
      * The Singleton's constructor should always be private to prevent direct
      * construction calls with the `new` operator.
+     *
+     * RU:
+     * Конструктор Одиночки всегда должен быть скрытым, чтобы предотвратить 
+     * прямое строительство, вызванное оператором new.
      */
     protected function __construct() { }
 
     /**
+     * EN:
      * Singletons should not be cloneable.
+     *
+     * RU:
+     * Одиночки не должны быть клонируемыми.
      */
     protected function __clone() { }
 
     /**
+     * EN:
      * Singletons should not be restorable from strings.
+     *
+     * RU:
+     * Одиночки не должны быть восстанавливаемыми из строк.
      */
     public function __wakeup()
     {
