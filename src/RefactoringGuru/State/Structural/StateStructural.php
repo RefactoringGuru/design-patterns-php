305696a6c93ca84440666db701b4e1b5dcb53e98
WTig <40171607+WTig@users.noreply.github.com>
2018-06-25 14:06:29 +0600

    Update StateStructural.php

--- src/RefactoringGuru/State/Structural/StateStructural.php
+++ src/RefactoringGuru/State/Structural/StateStructural.php
@@ -8,7 +8,7 @@ namespace RefactoringGuru\State\Structural;
  * Intent: Allow an object to alter its behavior when its internal state
  * changes. The object will appear to change its class.
  *
- * * RU: Паттерн Состояние
+ * RU: Паттерн Состояние
  *
  * Назначение: Позволяет объекту менять поведение при изменении его
  * внутреннего состояния. Кажется, что объект меняет свой класс.
@@ -27,7 +27,11 @@ namespace RefactoringGuru\State\Structural;
 class Context
 {
     /**
+     * EN:
      * @var State A reference to the current state of the Context.
+     *
+     * RU:
+     *  @var State Ссылка на текущее состояние Контекста.
      */
     private $state;
 
@@ -40,7 +44,11 @@ class Context
     }
 
     /**
+     * EN:
      * The Context allows changing the State object at runtime.
+     *
+     * RU:
+     * Контекст позволяет изменять объект Состояние во время выполнения.
      *
      * @param State $state
      */
@@ -52,7 +60,11 @@ class Context
     }
 
     /**
+     * EN:
      * The Context delegates part of its behavior to the current State object.
+     *
+     * RU:
+     * Контекст делегирует часть своего поведения текущему объекту Состояние.
      */
     public function request1()
     {
@@ -66,10 +78,17 @@ class Context
 }
 
 /**
+ * EN:
  * The base State class declares methods that all Concrete State should
  * implement and also provides a backreference to the Context object, associated
  * with the State. This backreference can be used by States to transition the
  * Context to another State.
+ *
+ * RU:
+ * Базовый класс Состояние объявляет методы, которые должны реализовывать
+ * все Конкретные Состояния, а также предоставляет обратную ссылку на объект Контекст,
+ * связанным с Состоянием. Эта обратная ссылка может использоваться Состояниями
+ * для передачи Контекста другому Состоянию.
  */
 abstract class State
 {
@@ -89,8 +108,12 @@ abstract class State
 }
 
 /**
+ * EN:
  * Concrete States implement various behaviors, associated with a state of the
  * Context.
+ *
+ * RU:
+ * Конкретные Состояния реализуют различные модели поведения, связанные с состоянием Контекста.
  */
 class ConcreteStateA extends State
 {
@@ -123,7 +146,9 @@ class ConcreteStateB extends State
 }
 
 /**
- * The client code.
+ * EN: The client code.
+ *
+ * RU: Клиентский код.
  */
 $context = new Context(new ConcreteStateA());
 $context->request1();
