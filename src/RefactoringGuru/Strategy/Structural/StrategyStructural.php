de9e02396e5139b3680020d8f2fdf9f75f49d4b7
WTig <40171607+WTig@users.noreply.github.com>
2018-06-25 17:07:59 +0600

    Update StrategyStructural.php

--- src/RefactoringGuru/Strategy/Structural/StrategyStructural.php
+++ src/RefactoringGuru/Strategy/Structural/StrategyStructural.php
@@ -26,15 +26,26 @@ namespace RefactoringGuru\Strategy\Structural;
 class Context
 {
     /**
+     * EN:
      * @var Strategy The Context maintains a reference to one of the Strategy
      * objects. The Context does not know the concrete class of a strategy. It
      * should work with all strategies via the Strategy interface.
+     *
+     * RU:
+     * @var Strategy Контекст хранит ссылку на один из объектов Стратегии. 
+     * Контекст не знает конкретного класса стратегии. Он должен работать со всеми
+     * стратегиями через интерфейс Стратегии.
      */
     private $strategy;
 
     /**
+     * EN:
      * Usually, the Context accepts a strategy through the constructor, but also
      * provides a setter to change it at runtime.
+     *
+     * RU:
+     * Обычно Контекст принимает стратегию через конструктор, а также предоставляет
+     * сеттер для изменения её во время выполнения.
      *
      * @param Strategy $strategy
      */
@@ -44,7 +55,11 @@ class Context
     }
 
     /**
+     * EN:
      * Usually, the Context allows replacing a Strategy object at runtime.
+     *
+     * RU:
+     * Обычно Контекст позволяет заменить объект Стратегии во время выполнения.
      *
      * @param Strategy $strategy
      */
@@ -54,8 +69,12 @@ class Context
     }
 
     /**
+     * EN:
      * The Context delegates some work to the Strategy object instead of
      * implementing multiple versions of the algorithm on its own.
+     *
+     * RU:
+     *
      */
     public function doSomeBusinessLogic()
     {
